801e9f0f80b162f5e8d0c4fe7205e4fd8e2dd51f
Apollos <severeapollos@gmail.com>
2024-02-22 17:24:07 -0500

    update

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,18 +1,7 @@
-import { auth } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
-import { isTeacher } from "@/lib/teacher";
- 
 const f = createUploadthing();
  
-const handleAuth = () => {
-  const { userId } = auth();
-  const isAuthorized = isTeacher(userId);
-
-  if (!userId || !isAuthorized) throw new Error("Unauthorized");
-  return { userId };
-}
-
 export const ourFileRouter = {
   courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
     // .middleware(() => handleAuth())
